a2bc4375b4f30fdc3784f7f5da78cba6cf73f466
Wisdom-Daniel <44666155+dew-dr0p@users.noreply.github.com>
2023-08-22 17:28:34 +0100

    Prepared Base Project Elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#EB3F57',
+        darkGray: '#454654',
+        background: '#F5F5FA',
+        gradientOne: {
+          a: '#E83F57',
+          b: '#8C3945'
+        },
+        gradientTwo: {
+          a: '#DE3C5D',
+          b: '#110943'
+        }
+      }
+    },
   },
   plugins: [],
 }
